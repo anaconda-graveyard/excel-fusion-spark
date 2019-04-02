4e321fd273ea178fdd9cddf9a12040fe05112c5e
Rudy Pena <rpena@anaconda.com>
2019-04-02 11:10:16 -0500

    adding message servcei for inputs and data

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, EventEmitter, Input } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { debounceTime, distinctUntilChanged, filter, tap, map} from 'rxjs/operators';
-
-// import { RepoPackagesApiService } from '@app/api'
+import { MessageService } from '../services/message.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -10,13 +10,24 @@ import { debounceTime, distinctUntilChanged, filter, tap, map} from 'rxjs/operat
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-    dataSet = [];
+    dataSet: any;
     loading: boolean;
+    //
+    message: any; // searchableFunctions
+    subscription: Subscription;
 
     constructor(
-        private router: Router,
+      private router: Router,
+      private messageService: MessageService
     ) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+      this.subscription = this.messageService
+          .getSearchableFunctions()
+          .subscribe((message) => {
+            console.log('search component::subscripted to getSearchableFunctions: ', message);
+            this.dataSet = message;
+          });
+    }
 
 }
