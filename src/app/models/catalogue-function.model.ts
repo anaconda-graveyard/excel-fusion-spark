bf47227e0b2494adec413d4f86e5ed07e35d011f
Rudy Pena <rpena@anaconda.com>
2019-04-18 11:01:29 -0500

    adding \starred\ prop to model -- future use:

--- src/app/models/catalogue-function.model.ts
+++ src/app/models/catalogue-function.model.ts
@@ -1,4 +1,4 @@
-import { CatalogFunctionMeta } from './catalogue-function-meta.model'
+import { CatalogFunctionMeta } from './catalogue-function-meta.model';
 
 export class CatalogFunction {
   catalogue: string;
@@ -7,6 +7,7 @@ export class CatalogFunction {
   tags: string[];
   url: string;
   meta: CatalogFunctionMeta[];
+  starred: boolean;
 
   constructor(
     catalogue: string,
@@ -14,7 +15,8 @@ export class CatalogFunction {
     name: string,
     tags: string[],
     url: string,
-    meta: CatalogFunctionMeta[]
+    meta: CatalogFunctionMeta[],
+    starred: boolean
   ) {
     this.catalogue = catalogue;
     this.description = description;
@@ -22,5 +24,6 @@ export class CatalogFunction {
     this.tags = tags;
     this.url = url;
     this.meta = meta;
+    this.starred = starred;
   }
 }
